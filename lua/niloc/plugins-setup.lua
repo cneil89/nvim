bee0484c99f85b7270201ff810feee204017fecd
Colin N <colin.neil18@gmail.com>
2023-05-02 00:52:37 -0400

    change colortheme

--- lua/niloc/plugins-setup.lua
+++ lua/niloc/plugins-setup.lua
@@ -33,7 +33,7 @@ return packer.startup(function(use)
 
     use { "nvim-lua/plenary.nvim" } 
     
-    use { "ellisonleao/gruvbox.nvim" }
+    use { "bluz71/vim-nightfly-colors", as = "nightfly" }
 
     use { "theprimeagen/harpoon" }
 
