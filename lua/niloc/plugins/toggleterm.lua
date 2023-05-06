e1ce0f9e6888942fc605078ff83080335050c89a
Colin N <colin.neil18@gmail.com>
2023-05-05 23:40:37 -0400

    disabled toggle term in insert mode

--- lua/niloc/plugins/toggleterm.lua
+++ lua/niloc/plugins/toggleterm.lua
@@ -2,4 +2,6 @@
 require("toggleterm").setup{
     direction = "float",
     open_mapping = [[<leader>j]],
+    insert_mappings = false, -- whether or not the open mapping applies in insert mode
+    terminal_mappings = true,
 }
