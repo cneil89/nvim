03a479e10766bf7e2be0974b630babafabb70229
Colin Neil <colin.neil18@gmail.com>
2024-01-08 10:17:15 -0500

    fix typo

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@ if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
         "git",
         "clone",
-        "--flither=blob:none",
+        "--fliter=blob:none",
         "https://github.com/folke/lazy.nvim.git",
         "--branch=stable", -- latest stable release
         lazypath,
