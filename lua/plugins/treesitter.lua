3ab1f43d3974cfff576ab4b5b986a5e293397766
Colin Neil <colin.neil18@gmail.com>
2024-01-10 20:42:52 -0500

    add gofmt to null-ls

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
 		local config = require("nvim-treesitter.configs")
 		config.setup({
 			auto_install = true,
-			-- ensure_installed = { "lua", "go", "cpp", "cmake" },
+			ensure_installed = { "lua", "go", "cpp", "cmake" },
 			highlight = { enable = true },
 			indent = { enable = true },
 		})
