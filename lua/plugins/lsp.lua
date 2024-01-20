a38d0b01d5de7430ec5d6215e51558b87398277b
Colin Neil <colin.neil18@gmail.com>
2024-01-20 01:22:54 -0500

    keybinds

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,22 +1,27 @@
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-    },
-    config = function()
-        require("mason").setup()
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                "gopls",
-                "clangd",
-                "cmake",
-            },
-            handlers = {
-                function (server_name)
-                    require("lspconfig")[server_name].setup({})
-                end
-            }
-        })
-    end
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+	},
+	config = function()
+		require("mason").setup()
+		require("mason-lspconfig").setup({
+			ensure_installed = {
+				"gopls",
+				"clangd",
+				"cmake",
+			},
+			handlers = {
+				function(server_name)
+					require("lspconfig")[server_name].setup({})
+				end,
+			},
+		})
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+		vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, {})
+	end,
 }
