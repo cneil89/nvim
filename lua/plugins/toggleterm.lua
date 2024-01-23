635ae1ae552091b2052aaa3eb182e1b2d6645580
Colin Neil <colin.neil18@gmail.com>
2024-01-22 22:10:22 -0500

    completions, toggleter, zenmode changes

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,20 +1,19 @@
 return {
     "akinsho/toggleterm.nvim",
     version = "*",
-    opts = {
-
-    },
+    opts = {},
     config = function()
         require("toggleterm").setup({
-            vim.keymap.set("n", "<C-t>", ":ToggleTerm<CR>", {}),
-            vim.keymap.set("t", "<C-t>", [[<C-\><C-n>:ToggleTerm<CR>]], {}),
-            vim.keymap.set("t", "jj", [[<C-\><C-n>]], {}),
-            vim.keymap.set("t", "<ESC>", [[<C-\><C-n>]], {}),
-            vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], {}),
-            vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], {}),
-            vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], {}),
-            vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], {}),
-            vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], {}),
+            direction = "float"
         })
+        vim.keymap.set("n", "<C-t>", ":ToggleTerm<CR>", {})
+        vim.keymap.set("t", "<C-t>", [[<C-\><C-n>:ToggleTerm<CR>]], {})
+        vim.keymap.set("t", "jj", [[<C-\><C-n>]], {})
+        vim.keymap.set("t", "<ESC>", [[<C-\><C-n>]], {})
+        vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], {})
+        vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], {})
+        vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], {})
+        vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], {})
+        vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], {})
     end,
 }
--- lua/plugins/zenmode.lua
+++ lua/plugins/zenmode.lua
@@ -1,8 +1,12 @@
 return {
     "folke/zen-mode.nvim",
+    lazy = true,
     opts = {
         -- your configuration comes here
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
     },
+    keys = {
+        {"<leader>zz", "<cmd>ZenMode<cr>", mode = "n", desc = "Toggle ZenMode"}
+    }
 }
