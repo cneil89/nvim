5bee8df80e8289afbae57e7069a01fde79cbee6d
Colin Neil <colin.neil18@gmail.com>
2024-01-23 23:31:12 -0500

    autocmds

--- init.lua
+++ init.lua
@@ -1,15 +1,32 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("vim-options")
 require("lazy").setup("plugins")
+
+local NilocGroup = vim.api.nvim_create_augroup("NilocGroup", { clear = true })
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+	pattern = "*",
+	group = NilocGroup,
+	callback = function()
+		vim.lsp.buf.format()
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+	pattern = "*",
+	group = NilocGroup,
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,27 +1,39 @@
 return {
-	"neovim/nvim-lspconfig",
-	dependencies = {
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-	},
-	config = function()
-		require("mason").setup()
-		require("mason-lspconfig").setup({
-			ensure_installed = {
-				"gopls",
-				"clangd",
-				"cmake",
-			},
-			handlers = {
-				function(server_name)
-					require("lspconfig")[server_name].setup({})
-				end,
-			},
-		})
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-		vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-		vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-		vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, {})
-	end,
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+        local lspconfig = require("lspconfig")
+        require("mason").setup()
+        require("mason-lspconfig").setup({
+            ensure_installed = {
+                "gopls",
+                "clangd",
+                "cmake",
+            },
+            handlers = {
+                function(server_name)
+                    lspconfig[server_name].setup({})
+                end,
+            },
+        })
+
+        lspconfig.lua_ls.setup({
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        globals = { "vim" },
+                    },
+                },
+            },
+        })
+
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+        vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, {})
+    end,
 }
