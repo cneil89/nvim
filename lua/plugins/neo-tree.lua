6635c9a3362736d3c4639e476ef20404df3a464d
Colin Neil <colin.neil18@gmail.com>
2024-01-25 23:28:37 -0500

    lazy load neotree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,13 +1,17 @@
 return {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
+    lazy = true,
+    keys = {
+        { "<C-b>", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
+    },
     dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-tree/nvim-web-devicons",
         "MunifTanjim/nui.nvim",
     },
     config = function()
-        vim.keymap.set("n", "<C-b>", "<CMD>Neotree toggle<CR>", {})
+        -- vim.keymap.set("n", "<C-b>", "<CMD>Neotree toggle<CR>", {})
         -- vim.keymap.set('n', '<leader>b', ':Neotree filesystem reveal left<CR>', {})
         vim.keymap.set("n", "<leader>B", ":Neotree buffers reveal float<CR>", {})
         require("neo-tree").setup({
