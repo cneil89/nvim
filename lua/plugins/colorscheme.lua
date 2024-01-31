0af933e0407b3018f9d9c29ff09aced8675ba393
Colin Neil <colin.neil18@gmail.com>
2024-01-31 13:49:03 -0500

    theme overriding

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,20 @@
 return {
-	"NLKNguyen/papercolor-theme",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		vim.o.background = "dark"
-		vim.cmd.colorscheme("PaperColor")
-	end,
+    "NLKNguyen/papercolor-theme",
+    lazy = false,
+    priority = 1000,
+    init = function()
+        vim.g.PaperColor_Theme_Options = {
+            theme = {
+                ["default.dark"] = {
+                    override = {
+                        error_bg = { "#1c1c1c", "234" },
+                    },
+                },
+            },
+        }
+    end,
+    config = function()
+        vim.o.background = "dark"
+        vim.cmd.colorscheme("PaperColor")
+    end,
 }
