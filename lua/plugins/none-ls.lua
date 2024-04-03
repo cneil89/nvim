59ee2a2b2fac3025f95900dc39726121475e1a83
Colin Neil <colin.neil18@gmail.com>
2024-04-02 23:10:53 -0400

    sql formatting

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -9,6 +9,9 @@ return {
                 null_ls.builtins.formatting.gofmt,
                 null_ls.builtins.formatting.goimports,
                 null_ls.builtins.formatting.templ,
+                null_ls.builtins.formatting.sqlfluff.with({
+                    extra_args = { "--dialect", "postgres" },
+                }),
                 null_ls.builtins.diagnostics.cppcheck.with({
                     extra_args = { "--language=c++", "--project=compile_commands.json" },
                 }),
